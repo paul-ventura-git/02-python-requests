89874483647e6ba307711d1b1cff106a76826ed6
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-08 14:29:33 -0500

    Nestjs more

--- api-GET-request.py
+++ api-GET-request.py
@@ -9,7 +9,3 @@ response = requests.get(url)
 # Print the response
 response_json = response.json()
 print(response_json)
-
-"""
-{'userId': 1, 'id': 1, 'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', 'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
-"""
